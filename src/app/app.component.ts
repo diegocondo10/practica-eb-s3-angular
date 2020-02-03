45d4ab590e5471cdc44574190dea4558cb92fd06
Diego Condo <diegocondo10@yahoo.es>
2020-02-02 23:01:40 -0500

    .
    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,45 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'practica-eb-s3-angular';
+
+  public archivo = null;
+
+  public imagenDeS3 = null;
+
+  constructor(
+    private http: HttpClient
+  ) {
+
+  }
+
+  changeInput(event) {
+    this.archivo = event.target.files[0]
+  }
+
+
+  async subirImagen() {
+
+    if (this.archivo) {
+      this.imagenDeS3 = null;
+      const body = new FormData();
+      body.append('imagen', this.archivo)
+
+      const respuesta = await this.http.post(
+        'http://practicatendencias-env.bmcingrtpt.us-east-1.elasticbeanstalk.com/subir-imagen',
+        body
+      )
+        .toPromise()
+      this.imagenDeS3 = respuesta['url'];
+    }
+
+    /*
+        this.http.get('http://127.0.0.1:8000/subir-imagen')
+          .subscribe(respuesta => {
+            console.log(respuesta)
+          })
+    */
+
+  }
+
+
 }
